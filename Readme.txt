98b0380b08b33ddd726ab4d652a16a7d1cd652cf
Lyuben <lubo@lyuben.laptop>
2020-09-18 22:14:30 +0200

    put all files in the main Repo

---
+++ Readme.txt
@@ -0,0 +1,9 @@
+Project: database
+App: website
+
+Super user: lubo
+
+how to run:
+source venv/bin/activate
+cd database
+python manage.py runserver
---
+++ website/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from website.models import *
+
+# Register your models here.
+admin.site.register(ProjectManagerModel)
+admin.site.register(ProjectModel)
+admin.site.register(CustomerModel)
+#admin.site.register(InvoiceModel)
+admin.site.register(ServiceModel)
+admin.site.register(Employees)
